42bf762f74cb4cb4f4ebf220d63ab92db035f207
yb00 <40061395+yb00@users.noreply.github.com>
2020-09-27 19:11:11 -0300

    Add css to pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,8 +4,12 @@ import Layout from "../components/layout"
 const Blog = () => {
   return (
     <Layout>
-      <div>
-        <h3>Blog</h3>
+      <div
+        className="box"
+        style={{ display: "flex", flexDirection: "column", flexGrow: "1" }}
+      >
+        <h3 className="box__title">My Blog</h3>
+        <div className="box__body"></div>
         <button style={{ width: "120px" }}>Add Post</button>
       </div>
     </Layout>
--- src/pages/merch.js
+++ src/pages/merch.js
@@ -3,11 +3,49 @@ import Layout from "../components/layout"
 const Merch = () => {
   return (
     <Layout>
-      <div>
-        <h3>My Merch</h3>
+      <div
+        className="box"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: "1",
+          textAlign: "center",
+          margin: "auto",
+        }}
+      >
+        <h3 className="box__title">Merchandise</h3>
+        <div
+          className="box__body"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <div className="box" style={{ border: "2px solid grey" }}>
+            <img src="https://via.placeholder.com/150x200" />
+          </div>
+          <div className="box" style={{ border: "2px solid grey" }}>
+            <img src="https://via.placeholder.com/150x200" />
+          </div>
+          <div className="box" style={{ border: "2px solid grey" }}>
+            <img src="https://via.placeholder.com/150x200" />
+          </div>
+          <div className="box" style={{ border: "2px solid grey" }}>
+            <img src="https://via.placeholder.com/150x200" />
+          </div>
+        </div>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query MyMerchQuery {
+    image: file(base: { eq: "album.jpg" }) {
+      publicURL
+    }
+  }
+`
 
 export default Merch
