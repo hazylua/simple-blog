aeb29358b2a52e5082baa2b318c6c069e555fed8
yb00 <40061395+yb00@users.noreply.github.com>
2020-09-27 19:10:35 -0300

    Remove test style

--- src/components/main.js
+++ src/components/main.js
@@ -2,15 +2,9 @@ import React from "react"
 
 import "./styles/main.css"
 
-const contrastColor = "#03fc49"
-
-const testStyle = {
-  border: `3px solid ${contrastColor}`,
-}
-
 const Main = ({ children }) => {
   return (
-    <div className="main-container" style={testStyle}>
+    <div className="main-container">
       <main className="main-box">{children}</main>
     </div>
   )
