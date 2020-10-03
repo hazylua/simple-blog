aaef28873042256e3857e4cc6b9630115a975ea9
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-02 22:02:31 -0300

    Add mobnav toggle component

--- src/components/header.js
+++ src/components/header.js
@@ -6,73 +6,86 @@ import { AiOutlineSearch, AiOutlineBars } from "react-icons/ai"
 
 import "./styles/header.css"
 
-const MobileNavbar = props => {
-  const [display, setDisplay] = useState([false])
+const NavbarMenu = props => {
+  return (
+    <nav
+      className="mobnavbar-container"
+      style={{
+        position: "absolute",
+        top: "88px",
+        transform: "translateX(-90%)",
+      }}
+    >
+      <Link className="mobnavbar-link" to="/">
+        Home
+      </Link>
+
+      <Link className="mobnavbar-link" to="/blog">
+        Blog
+      </Link>
+
+      <Link
+        className="mobnavbar-link"
+        activeClassName="active-link"
+        to="/merch"
+      >
+        Merch
+      </Link>
+
+      <Link className="mobnavbar-link" to="/about">
+        About
+      </Link>
+
+      <Link className="mobnavbar-link" to="/contact">
+        Contact
+      </Link>
+    </nav>
+  )
+}
+
+const MobNavToggle = props => {
+  const [display, setDisplay] = useState(false)
   return (
     <>
-      {display && (
-        <nav className="mobnavbar-container">
-          <Link className="mobnavbar-link" to="/">
-            Home
-          </Link>
-
-          <Link className="mobnavbar-link" to="/blog">
-            Blog
-          </Link>
-
-          <Link
-            className="mobnavbar-link"
-            activeClassName="active-link"
-            to="/merch"
-          >
-            Merch
-          </Link>
-
-          <Link className="mobnavbar-link" to="/about">
-            About
-          </Link>
-
-          <Link className="mobnavbar-link" to="/contact">
-            Contact
-          </Link>
-        </nav>
-      )}
+      <div id="header-mobile-nav">
+        <AiOutlineBars
+          style={{
+            display: "block",
+            margin: "auto",
+          }}
+          color={"white"}
+          size={30}
+          onClick={() => setDisplay(!display)}
+        />
+      </div>
+      {display && props.children}
     </>
   )
 }
 
 const Header = ({ siteTitle }) => (
-  <>
-    <header className="header-container">
-      <h1 style={{ marginBottom: "5px" }}>
-        <Link to="/" className="header-title">
-          {siteTitle}
-        </Link>
-      </h1>
-      <div className="header-search-wrapper">
-        <input className="header-search-input" />
-        <div id="header-search-icon">
-          <AiOutlineSearch
-            style={{
-              display: "block",
-              margin: "auto",
-            }}
-            size={30}
-          />
-        </div>
-        <div id="header-mobile-nav">
-          <AiOutlineBars
-            style={{
-              display: "block",
-              margin: "auto",
-            }}
-            color={"white"}
-            size={30}
-          />
-        </div>
+  <header className="header-container">
+    <h1 style={{ marginBottom: "5px" }}>
+      <Link to="/" className="header-title">
+        {siteTitle}
+      </Link>
+    </h1>
+    <div className="header-search-wrapper">
+      <input className="header-search-input" />
+      <div id="header-search-icon">
+        <AiOutlineSearch
+          style={{
+            display: "block",
+            margin: "auto",
+          }}
+          size={30}
+        />
       </div>
-    </header>
-  </>
+      <MobNavToggle>
+        <NavbarMenu />
+      </MobNavToggle>
+    </div>
+  </header>
 )
 
 Header.propTypes = {
