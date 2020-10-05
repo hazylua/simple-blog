e99649248bd5a2ebd50ddd121edd624fb1f3777c
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-04 21:45:49 -0300

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        height: "100px",
+        borderTop: "3px solid rgb(51, 19, 82)",
+        background: "rebeccapurple",
+      }}
+    ></div>
+  )
+}
+
+export default Footer
