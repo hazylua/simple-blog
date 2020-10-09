b3de433159384e5e519c1d990f6b8efc5bc6a9ca
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-09 16:47:19 -0300

    Add about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,32 @@ import React from "react"
 import Layout from "../components/layout"
 
 const About = () => {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <div
+        className="container"
+        style={{ maxWidth: "1000px", margin: "auto", marginTop: "1.45rem" }}
+      >
+        <h3>About</h3>
+        <p>So, hi!</p>
+        <p>
+          Nostrud elit in reprehenderit et voluptate culpa commodo velit id
+          nostrud.
+        </p>
+        <ul>
+          <li>Nostrud ad sit non quis duis nostrud labore eu cupidatat qui;</li>
+          <li>Nostrud ad sit non quis duis nostrud labore eu cupidatat qui;</li>
+          <li>Nostrud ad sit non quis duis nostrud labore eu cupidatat qui;</li>
+          <li>Nostrud ad sit non quis duis nostrud labore eu cupidatat qui;</li>
+        </ul>
+        <p>
+          In id voluptate aliquip dolore duis sint quis amet. Tempor magna
+          commodo veniam excepteur excepteur incididunt. In pariatur Lorem duis
+          ipsum eu. Culpa commodo dolore proident exercitation duis.
+        </p>
+      </div>
+    </Layout>
+  )
 }
 
 export default About
