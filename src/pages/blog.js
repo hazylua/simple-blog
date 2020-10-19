94c5137fa01ab5db1915974555c712f524078b7f
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-18 23:33:25 -0300

    Comment postbuilder for the moment

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql, Link } from "gatsby"
-import PostBuilder from "./postbuilder"
+// import PostBuilder from "./postbuilder"
 
 import "../components/styles/main.css"
 
