1d7c6a191a6a07db8369ee5590939a6106c0f0b5
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-18 23:51:50 -0300

    Add page selector component as PageRow

---
+++ src/components/PageRow/PageRow.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import "./PageRow.css"
+
+const PageSelect = ({ pageValue, setPage }) => {
+  return (
+    <button className="page-select" onClick={() => setPage(pageValue)}>
+      {pageValue}
+    </button>
+  )
+}
+
+const PageRow = ({ maxResultsPerPage, itemsNum, setPage }) => {
+  var buttons = []
+  for (var i = 1; i <= Math.ceil(itemsNum / maxResultsPerPage); i++) {
+    buttons.push(<PageSelect pageValue={i} setPage={setPage} />)
+  }
+  return (
+    <div>
+      {buttons.map(button => {
+        return button
+      })}
+    </div>
+  )
+}
+
+export default PageRow
