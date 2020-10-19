01a807dd49b7ad57ccffd338c68de9d11f67ed47
yb00 <40061395+yb00@users.noreply.github.com>
2020-10-18 23:52:41 -0300

    Use page selector component in search

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
+import PageRow from "../components/PageRow"
 
 const Result = ({ title, date, excerpt, path }) => {
   return (
