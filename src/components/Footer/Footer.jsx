91b85d91711627d5b4d905278afc54cfa146eb2c
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-03 22:10:29 -0300

    Change footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,42 +1,34 @@
 import React from "react"
 import { AiFillGithub } from "react-icons/ai"
 
-const Links = props => {
+import "./Footer.css"
+
+const GitLink = ({ url }) => {
   return (
-    <div style={{ margin: "0.75rem 0" }}>
-      <a
-        style={{ textDecoration: "none", color: "rgb(245, 245, 245)" }}
-        href={props.repo}
-      >
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          <AiFillGithub
-            style={{ display: "block", margin: "auto 0.25rem auto 0" }}
-          />{" "}
-          Github
-        </div>
+    <div className="footer-link-wrapper">
+      <a href={url}>
+        <AiFillGithub className="footer-link__icon" />
       </a>
+      Github
     </div>
   )
 }
 
 const Copyright = () => {
-  return <div>© 2020 - Copyright Placeholder</div>
+  return (
+    <div>
+      <span className="footer-copyright__symbol">©</span> 2020 - Copyright
+      Placeholder
+    </div>
+  )
 }
 
 const Footer = ({ siteTitle, siteRepo }) => {
   return (
-    <div
-      style={{
-        background: "#562996",
-        borderTop: "3px solid rgb(51, 19, 82)",
-        color: "rgb(245, 245, 245)",
-        marginTop: "1.45rem",
-        padding: "1.45rem 2rem",
-      }}
-    >
-      <div style={{ margin: "auto", maxWidth: "80%" }}>
+    <div className="footer">
+      <div className="footer-wrapper">
         {siteTitle}
-        <Links repo={siteRepo} />
+        <GitLink url={siteRepo} />
         <Copyright />
       </div>
     </div>
