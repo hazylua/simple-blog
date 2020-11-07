122d367a4d0a66517db6b53f50597f70907f3185
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-06 23:07:46 -0300

    Add auth logic

--- server/auth.js
+++ server/auth.js
@@ -4,38 +4,23 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const webtoken = require("jsonwebtoken")
-const { assert } = require("console")
+
+// MongoDB.
+const mongoose = require("mongoose")
+
+// Routes.
+const routes = require("./routes")
 
 const app = express()
-const port = process.env.AUTH_PORT
 
-const MongoClient = require("mongodb").MongoClient
-MongoClient.connect(process.env.DB_URL, (err, db) => {
-  if (err) throw err
-  console.log(`Connected to MongoDB at ${process.env.DB_URL}.`)
+mongoose
+  .connect(`${process.env.DB_URL}`)
+  .then(() => console.log(`Connected to MongoDB.`))
+  .catch(err => console.error(`Error:\n${err}`))
 
-  db.close()
-})
+app.use(express.json())
+app.use("/api/users", routes.user)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
-
-app.post("/auth", (req, res) => {})
-
-function webtokenSignUser(user) {
-  return webtoken.sign(user, config.authentication.webtokenSecret, {
-    expiresIn: "30m",
-  })
-}
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}.`)
+app.listen(process.env.AUTH_PORT, () => {
+  console.log(`Server started on port ${process.env.AUTH_PORT}.`)
 })
