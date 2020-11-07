f506bd8f93f4605787690d59a3ae289b697f9b1d
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-06 23:02:09 -0300

    Fix joi validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,38 +1,45 @@
-const Joi = require('joi')
-const mongoose = require('mongoose')
+const Joi = require("joi")
+const mongoose = require("mongoose")
 
 const lenMin = 5
 const lenMax = 255
 
-const User = mongoose.model('User', new mongoose.Schema({
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     name: {
-        type: String,
-        require: true,
-        lenMin: lenMin,
-        lenMax: lenMax
+      type: String,
+      require: true,
+      lenMin: lenMin,
+      lenMax: lenMax,
     },
     email: {
-        type: String,
-        require: true,
-        lenMin: lenMin,
-        lenMax: lenMax,
-        unique: true
+      type: String,
+      require: true,
+      lenMin: lenMin,
+      lenMax: lenMax,
+      unique: true,
     },
     password: {
-        type: String,
-        require: true,
-        lenMin: lenMin,
-        lenMax: lenMax
+      type: String,
+      require: true,
+      lenMin: lenMin,
+      lenMax: lenMax,
+    },
+  })
+)
 
-    }
-}))
-
-const validateUser = (User) => {
-    const schema = {
-        name: Joi.string().min(lenMin).max(lenMax).required(),
-        email: Joi.string().min(lenMin).max(lenMax).required().email(),
-        password: Joi.string().min(lenMin).max(lenMax).required()
-    }
+const validateUser = bodyUser => {
+  const schema = Joi.object({
+    name: Joi.string().min(lenMin).max(lenMax).required(),
+    email: Joi.string().min(lenMin).max(lenMax).required().email(),
+    password: Joi.string().min(lenMin).max(lenMax).required(),
+  })
+  // synchronous validate.
+  return schema.validate(bodyUser)
+  // asynchronous validate.
+  // return schema.validate
+  // schema.validateAsync(bodyUser)
 }
 
 exports.User = User
