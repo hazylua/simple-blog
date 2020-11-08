f8ad63662eedbf7d3e1b7316bfa8cfeda5a1b9a6
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-07 23:26:22 -0300

    Encrypt user data

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,17 @@
-const { User, validate } = require("../models/user")
+const { User, validateUser } = require("../models/user")
 const express = require("express")
+const _ = require("lodash")
 const router = express.Router()
 
+const SALT_FACTOR = 10
+
+const bcrypt = require("bcrypt")
+
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body)
-  if (error) {
-    return res.status(400).send(error.details[0].message)
+  // Check for errors.
+  const { err } = validateUser(req.body)
+  if (err) {
+    return res.status(400).send(err.details[0].message)
   }
 
   let user = await User.findOne({ email: req.body.email })
@@ -17,8 +23,12 @@ router.post("/", async (req, res) => {
       email: req.body.email,
       password: req.body.password,
     })
+    user = new User(_.pick(req.body, ["name", "email", "password"]))
+    const salt = await bcrypt.genSalt(SALT_FACTOR)
+    user.password = await bcrypt.hash(user.password, salt)
     await user.save()
-    res.send(user)
+
+    res.send(_.pick(user, ["_id", "name", "email"]))
   }
 })
 
