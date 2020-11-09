99bf576dd187d524de8cf978c46ae54020941666
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-08 21:04:51 -0300

    Fix unhandled rejection

--- server/routes/user.js
+++ server/routes/user.js
@@ -13,22 +13,25 @@ router.post("/", async (req, res) => {
   if (err) {
     return res.status(400).send(err.details[0].message)
   }
+  try {
+    let user = await User.findOne({ email: req.body.email })
+    if (user) {
+      return res.status(400).send("Email is already registered.")
+    } else {
+      user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      user = new User(_.pick(req.body, ["name", "email", "password"]))
+      const salt = await bcrypt.genSalt(SALT_FACTOR)
+      user.password = await bcrypt.hash(user.password, salt)
+      await user.save()
 
-  let user = await User.findOne({ email: req.body.email })
-  if (user) {
-    return res.status(400).send("Email is already registered.")
-  } else {
-    user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-    user = new User(_.pick(req.body, ["name", "email", "password"]))
-    const salt = await bcrypt.genSalt(SALT_FACTOR)
-    user.password = await bcrypt.hash(user.password, salt)
-    await user.save()
-
-    res.send(_.pick(user, ["_id", "name", "email"]))
+      res.send(_.pick(user, ["_id", "name", "email"]))
+    }
+  } catch (err) {
+    console.log(err)
   }
 })
 
