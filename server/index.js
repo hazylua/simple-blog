c2c15ea71c6ee0f36d7511c8a1870097c90730e8
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-08 21:05:53 -0300

    Add default get

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const cors = require("cors")
 
 // MongoDB.
 const mongoose = require("mongoose")
@@ -13,8 +14,14 @@ const app = express()
 mongoose
   .connect(`${process.env.DB_URL}`)
   .then(() => console.log(`Connected to MongoDB.`))
-  .catch(err => console.error(`Error:\n${err}`))
+  .catch(err => console.error(`Error:\n${err}.`))
 
+// Default response from '/'.
+app.get("/", (req, res) => {
+  res.send(`Express server at port ${process.env.AUTH_PORT}.`)
+})
+
+app.use(cors())
 app.use(express.json())
 app.use("/api/users", routes.user)
 app.use("/api/auth", routes.auth)
