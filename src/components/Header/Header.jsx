8c8f617c55099d5a27735438e227ad0e2ed27ff4
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-15 23:47:47 -0300

    Decouple searchbar from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,127 +1,18 @@
-import { Link, navigate } from "gatsby"
+import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
-
-import { AiOutlineSearch, AiOutlineBars } from "react-icons/ai"
+import React from "react"
 
 import "./Header.css"
 
-const NavbarMenu = props => {
-  return (
-    <>
-      {props.display && (
-        <nav className="mobnavbar-container">
-          <Link className="mobnavbar-link" to="/">
-            Home
-          </Link>
-          <Link className="mobnavbar-link" to="/blog">
-            Blog
-          </Link>
-          <Link className="mobnavbar-link" to="/about">
-            About
-          </Link>
-          <Link className="mobnavbar-link" to="/contact">
-            Contact
-          </Link>
-        </nav>
-      )}
-    </>
-  )
-}
-
-const MobNavToggle = () => {
-  return (
-    <>
-      <div id="header-mobile-nav">
-        <AiOutlineBars
-          style={{
-            display: "block",
-            margin: "auto",
-          }}
-          color={"white"}
-          size={30}
-        />
-      </div>
-    </>
-  )
-}
-
-const SearchBar = ({
-  displayMenu,
-  setDisplayMenu,
-  handleChange,
-  handleEnterDown,
-}) => {
-  const [isFocused, setFocus] = useState(false)
-
-  return (
-    <div
-      className="header-search-wrapper"
-      onFocus={() => setFocus(true)}
-      onBlur={() => setFocus(false)}
-      style={
-        isFocused
-          ? {
-              outline: "none",
-              borderColor: "blue",
-              boxShadow: "0 0 10px #9ecaed",
-            }
-          : { border: "2px solid #dadada", borderRadius: "3px" }
-      }
-    >
-      <input
-        className="header-search-input"
-        onChange={handleChange}
-        onKeyDown={handleEnterDown}
-      />
-      <div id="header-search-icon">
-        <AiOutlineSearch
-          style={{
-            display: "block",
-            margin: "auto",
-          }}
-          size={30}
-        />
-      </div>
-      <div onClick={() => setDisplayMenu(!displayMenu)}>
-        <MobNavToggle />
-      </div>
-    </div>
-  )
-}
-
 const Header = ({ siteTitle }) => {
-  const [displayMenu, setDisplayMenu] = useState(false)
-  const [query, setQuery] = useState("")
-
-  const handleEnterDown = event => {
-    if (event.key === "Enter") {
-      event.preventDefault()
-      navigate("/search", { state: { query } })
-    }
-  }
-
-  const handleChange = event => {
-    setQuery(event.target.value)
-  }
-
   return (
-    <>
-      <header className="header-container">
-        <h1 style={{ marginBottom: "5px" }}>
-          <Link to="/" className="header-title">
-            {siteTitle}
-          </Link>
-        </h1>
-        <SearchBar
-          handleChange={handleChange}
-          displayMenu={displayMenu}
-          setDisplayMenu={setDisplayMenu}
-          handleEnterDown={handleEnterDown}
-        />
-      </header>
-      <NavbarMenu display={displayMenu} />
-    </>
+    <header className="header-container">
+      <h1 style={{ marginBottom: "5px" }}>
+        <Link to="/" className="header-title">
+          {siteTitle}
+        </Link>
+      </h1>
+    </header>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,31 +2,36 @@ import React from "react"
 
 import { Link } from "gatsby"
 
+import SearchBar from "src/components/SearchBar"
+
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav className="navbar-container">
-      <Link className="navbar-link" activeClassName="active-link" to="/">
-        Home
-      </Link>
-      <span className="navbar-separator">/</span>
-      <Link className="navbar-link" activeClassName="active-link" to="/blog">
-        Blog
-      </Link>
-      <span className="navbar-separator">/</span>
-      {/* <Link className="navbar-link" activeClassName="active-link" to="/merch">
-        Merch
-      </Link>
-      <span className="navbar-separator">/</span> */}
-      <Link className="navbar-link" activeClassName="active-link" to="/about">
-        About
-      </Link>
-      <span className="navbar-separator">/</span>
-      <Link className="navbar-link" activeClassName="active-link" to="/contact">
-        Contact
-      </Link>
-      <span className="navbar-separator">/</span>
+      <div className="links-wrapper">
+        <Link className="navbar-link" activeClassName="active-link" to="/">
+          Home
+        </Link>
+        <span className="navbar-separator">/</span>
+        <Link className="navbar-link" activeClassName="active-link" to="/blog">
+          Blog
+        </Link>
+        <span className="navbar-separator">/</span>
+        <Link className="navbar-link" activeClassName="active-link" to="/about">
+          About
+        </Link>
+        <span className="navbar-separator">/</span>
+        <Link
+          className="navbar-link"
+          activeClassName="active-link"
+          to="/contact"
+        >
+          Contact
+        </Link>
+        <span className="navbar-separator">/</span>
+      </div>
+      <SearchBar />
     </nav>
   )
 }
