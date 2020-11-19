2461075194decc747b8fd71f63d5e7dbc70ba54b
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-18 23:25:54 -0300

    Change form structure

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,55 +1,61 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "src/components/Layout"
 
 import axios from "axios"
 
 import "./styles/contact.css"
 
-const sendInfo = (mail, name, message) => {
-  try {
-    const response = axios.post("https://localhost:5000/api/contact", {
-      mail: mail,
-      subject: `Website Contact - ${name}`,
-      message: message,
-    })
-    alert(response.success)
-  } catch (err) {
-    alert(err)
-  }
-}
-
 const Contact = () => {
   const [name, setName] = useState("")
   const [mail, setMail] = useState("")
   const [message, setMessage] = useState("")
+  const [apiStatus, setApiStatus] = useState("")
+
+  const submitContact = async e => {
+    e.preventDefault()
+    const now = Date.now()
+    try {
+      const response = await axios.post("http://localhost:5000/api/contact", {
+        subject: `Website Contact - ${name}`,
+        mail: mail,
+        date: now,
+        message: message,
+      })
+      console.log(response.data)
+      setApiStatus("Contact form submitted successfully!")
+    } catch (err) {
+      setApiStatus(`An error has occurred.`)
+    }
+  }
 
   return (
     <Layout>
-      <div className="contact-container">
+      <div className="contact-container light-bg border">
         <h3>Contact</h3>
         <p>
           Want to get in contact with me? Fill the form below and I'll answer
           whatever you send me as soon as possible!
         </p>
-
-        <form
-          className="contact-form"
-          method="post"
-          action={() => sendInfo(mail, name, message)}
-        >
+        <form className="contact-form" onSubmit={submitContact}>
           <input
             placeholder="Your name"
+            value={name}
             onChange={e => setName(e.target.value)}
           ></input>
           <input
             placeholder="Email"
+            value={mail}
             onChange={e => setMail(e.target.value)}
           ></input>
           <textarea
             placeholder="Message"
+            value={message}
             onChange={e => setMessage(e.target.value)}
           ></textarea>
-          <button>Send!</button>
+          <button type="submit" value="Submit">
+            Send!
+          </button>
+          {apiStatus}
         </form>
       </div>
     </Layout>
