e62b3fb04f100204d900a371e3922f7461d9fc63
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-19 23:29:38 -0300

    Add submit notification

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react"
+
 import Layout from "src/components/Layout"
+import Snackbar from "src/components/Snackbar"
 
 import axios from "axios"
 
@@ -10,6 +12,7 @@ const Contact = () => {
   const [mail, setMail] = useState("")
   const [message, setMessage] = useState("")
   const [apiStatus, setApiStatus] = useState("")
+  const [sent, setSent] = useState(false)
 
   const submitContact = async e => {
     e.preventDefault()
@@ -21,9 +24,15 @@ const Contact = () => {
         date: now,
         message: message,
       })
-      console.log(response.data)
+      console.log(response.data, sent)
+      setSent(true)
+      setTimeout(() => setSent(false), 3000)
       setApiStatus("Contact form submitted successfully!")
     } catch (err) {
+      setSent(true)
+      setTimeout(() => {
+        setSent(false)
+      }, 3000)
       setApiStatus(`An error has occurred.`)
     }
   }
@@ -31,10 +40,10 @@ const Contact = () => {
   return (
     <Layout>
       <div className="contact-container light-bg border">
-        <h3>Contact</h3>
+        <h2>Contact</h2>
         <p>
-          Want to get in contact with me? Fill the form below and I'll answer
-          whatever you send me as soon as possible!
+          Want to get in contact with me? Just fill the form below with any
+          questions you have!
         </p>
         <form className="contact-form" onSubmit={submitContact}>
           <input
@@ -55,7 +64,14 @@ const Contact = () => {
           <button type="submit" value="Submit">
             Send!
           </button>
-          {apiStatus}
+          <Snackbar
+            open={sent}
+            top={"0"}
+            left={"50%"}
+            transform={"translateX(-50%)"}
+          >
+            {apiStatus}
+          </Snackbar>
         </form>
       </div>
     </Layout>
