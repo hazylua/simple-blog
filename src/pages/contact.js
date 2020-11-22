0c071db2a5d5582747fe04e9937a180a7a0ac87b
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-21 23:38:33 -0300

    Make form and snackbar a component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 
 import Layout from "src/components/Layout"
 import Snackbar from "src/components/Snackbar"
@@ -7,36 +7,66 @@ import axios from "axios"
 
 import "./styles/contact.css"
 
-const Contact = () => {
+const SubmitNotification = ({ children }) => {
+  return (
+    <Snackbar
+      top={"0"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+      open={false}
+    >
+      {children}
+    </Snackbar>
+  )
+}
+
+const ContactForm = () => {
   const [name, setName] = useState("")
   const [mail, setMail] = useState("")
   const [message, setMessage] = useState("")
-  const [apiStatus, setApiStatus] = useState("")
-  const [sent, setSent] = useState(false)
 
   const submitContact = async e => {
     e.preventDefault()
-    const now = Date.now()
     try {
       const response = await axios.post("http://localhost:5000/api/contact", {
         subject: `Website Contact - ${name}`,
         mail: mail,
-        date: now,
+        date: new Date.now(),
         message: message,
       })
-      console.log(response.data, sent)
-      setSent(true)
-      setTimeout(() => setSent(false), 3000)
-      setApiStatus("Contact form submitted successfully!")
+      console.log(response)
     } catch (err) {
-      setSent(true)
-      setTimeout(() => {
-        setSent(false)
-      }, 3000)
-      setApiStatus(`An error has occurred.`)
+      console.log(err)
     }
   }
 
+  return (
+    <form className="contact-form" onSubmit={submitContact}>
+      <input
+        placeholder="Your name"
+        value={name}
+        onChange={e => setName(e.target.value)}
+      ></input>
+      <input
+        placeholder="Email"
+        value={mail}
+        onChange={e => setMail(e.target.value)}
+      ></input>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      ></textarea>
+      <button type="submit" value="Submit">
+        Send!
+      </button>
+
+      <SubmitNotification>cool</SubmitNotification>
+    </form>
+  )
+}
+
+const Contact = () => {
   return (
     <Layout>
       <div className="contact-container light-bg border">
@@ -45,34 +75,7 @@ const Contact = () => {
           Want to get in contact with me? Just fill the form below with any
           questions you have!
         </p>
-        <form className="contact-form" onSubmit={submitContact}>
-          <input
-            placeholder="Your name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          ></input>
-          <input
-            placeholder="Email"
-            value={mail}
-            onChange={e => setMail(e.target.value)}
-          ></input>
-          <textarea
-            placeholder="Message"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-          ></textarea>
-          <button type="submit" value="Submit">
-            Send!
-          </button>
-          <Snackbar
-            open={sent}
-            top={"0"}
-            left={"50%"}
-            transform={"translateX(-50%)"}
-          >
-            {apiStatus}
-          </Snackbar>
-        </form>
+        <ContactForm />
       </div>
     </Layout>
   )
