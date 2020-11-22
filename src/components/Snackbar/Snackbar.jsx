4f480557d303d08453221d35644989e147665a1a
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-21 23:39:14 -0300

    Change snackbar variables

--- src/components/Snackbar/Snackbar.jsx
+++ src/components/Snackbar/Snackbar.jsx
@@ -1,12 +1,25 @@
-import { transform } from "lodash"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import "./Snackbar.css"
 
 const Snackbar = ({ children, top, bottom, left, right, open, transform }) => {
+  const [render, setRender] = useState(false)
+
+  useEffect(() => {
+    console.log(open)
+    if (open === true) {
+      setRender(true)
+      setTimeout(() => {
+        setRender(false)
+      }, 5000)
+    } else {
+      setRender(false)
+    }
+  }, [open])
+
   return (
     <React.Fragment>
-      {open ? (
+      {render ? (
         <div
           className="snackbar"
           style={{
