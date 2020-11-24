3615d5f3dd42bd4078dcf5d974fae0d4945e0653
yb00 <40061395+yb00@users.noreply.github.com>
2020-11-23 22:12:07 -0300

    Fix snackbar lifecycle and fade in/out animations

--- src/components/Snackbar/Snackbar.jsx
+++ src/components/Snackbar/Snackbar.jsx
@@ -2,37 +2,60 @@ import React, { useEffect, useState } from "react"
 
 import "./Snackbar.css"
 
-const Snackbar = ({ children, top, bottom, left, right, open, transform }) => {
+const Snackbar = ({ children, mount, setPending, displayTime }) => {
   const [render, setRender] = useState(false)
+  const [style, setStyle] = useState({
+    opacity: 0,
+    transition: "all 2s ease",
+    top: 0,
+    left: "50%",
+    transform: "translateX(-50%)",
+    marginTop: "5px",
+  })
 
   useEffect(() => {
-    if (open === true) {
-      setRender(true)
-      setTimeout(() => {
-        setRender(false)
-      }, 5000)
-    } else {
-      setRender(false)
+    // After setPending, display fade out animation and unmount the component.
+    if (!mount) {
+      setTimeout(() => unMountStyle(), 10)
+      setTimeout(() => setRender(false), 1000)
+      return
     }
-  }, [open])
+    if (mount) {
+      // Set the snackbar to render.
+      setRender(true)
+    } else if (!mount) {
+    }
+    if (render) {
+      // Once rendered, change styles.
+      setTimeout(() => mountStyle(), 10)
+      // After an amount of time set by displayTime has passed, set it to tell page that notification has been displayed.
+      setTimeout(() => setPending(), displayTime)
+    }
+  }, [mount, render])
+
+  const mountStyle = () => {
+    setStyle({ ...style, transition: "all 1s ease", opacity: 1 })
+  }
+  const unMountStyle = () => {
+    setStyle({ ...style, transition: "all 1s ease", opacity: 0 })
+  }
 
   return (
-    <React.Fragment>
-      {render ? (
-        <div
-          className="snackbar"
-          style={{
-            top: top,
-            bottom: bottom,
-            left: left,
-            right: right,
-            transform: transform,
-          }}
-        >
-          {children}
-        </div>
-      ) : null}
-    </React.Fragment>
+    render && (
+      <div
+        className="snackbar"
+        // style={{
+        //   top: top,
+        //   bottom: bottom,
+        //   left: left,
+        //   right: right,
+        //   transform: transform,
+        // }}
+        style={style}
+      >
+        {children}
+      </div>
+    )
   )
 }
 
