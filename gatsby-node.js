676a90f63ce60848d4ad87a5bfda175e7bc06aa3
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-10 23:31:02 -0300

    Add check to path names

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,25 +12,39 @@ const getAllPosts = async () => {
 }
 
 exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
   const PostTemplate = path.resolve("src/templates/blogPost.js")
+  const posts = await getAllPosts()
+
+  let paths = []
   const postPathPrefix = "posts/"
   const pathFormat = /[^ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-]/g
-  const { createPage } = actions
-  const posts = await getAllPosts()
+
   posts.map(post => {
     const slug =
       postPathPrefix +
       post.title.toLowerCase().replace(/ /g, "-").replace(pathFormat, "")
-    createPage({
-      path: slug,
-      component: PostTemplate,
-      context: {
-        title: post.title,
-        date: post.date,
-        author: post.author,
-        content: post.body,
-        comments: post.comments,
-      },
-    })
+    const date = new Date(post.date).toLocaleDateString("pt-br")
+
+    if (!paths.find(i => i == slug)) {
+      paths.push(slug)
+      createPage({
+        path: slug,
+        component: PostTemplate,
+        context: {
+          slug: slug,
+          date: date,
+          title: post.title,
+          author: post.author,
+          content: post.body,
+          comments: post.comments,
+        },
+      })
+    } else {
+      console.error(
+        "Path already exists. No duplicate post names are allowed. Check server."
+      )
+    }
   })
 }
