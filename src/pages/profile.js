e18fd9c3ce88ab7948c67f302f3d0ae838a4e2ff
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-11 23:25:38 -0300

    Rename profile

---
+++ src/pages/profile.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "src/components/Layout"
+
+import "./styles/profile.css"
+
+const Profile = () => {
+  return (
+    <Layout>
+      <div className="profile-container">
+        <h1>L</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default Profile
--- src/pages/profile/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import "./Profile.css"
-
-const Profile = () => {
-  return <div></div>
-}
-
-export default Profile
