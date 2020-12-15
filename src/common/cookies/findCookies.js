11c42b77b003a762c760a6c0c802a1f72063d05a
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-14 22:00:36 -0300

    Remove common folder

--- src/common/cookies/findCookies.js
+++
@@ -1,9 +0,0 @@
-export const findCookie = value => {
-  if (typeof value != "string") return console.log("Not a string.")
-  let cookies = document.cookie
-  cookies = cookies.split(";").find(row => row.startsWith(`${value}`))
-  if (cookies) {
-    console.log(cookies.split("=")[1])
-    return cookies.split("=")[1]
-  } else return ""
-}
