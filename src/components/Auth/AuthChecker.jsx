25a7bc6f4bdf3bb3b08bf1d473545b0aa8229315
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-15 23:10:18 -0300

    Check cookies on reload

---
+++ src/components/Auth/AuthChecker.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react"
+
+import PropTypes from "prop-types"
+
+import { authSession, leaveSession } from "src/store/actions"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
+
+import { tokenCheck } from "src/services/user-auth"
+import _ from "lodash"
+
+import { signUser, logoutUser } from "./userActions"
+
+const AuthChecker = ({ UserSession, actions, children }) => {
+  const { authSession, leaveSession } = actions
+
+  const getUser = async () => {
+    try {
+      const response = await tokenCheck()
+      const user_data = await response.data
+      const date_expire = new Date(user_data.exp * 1000)
+      signUser(
+        {
+          ..._.pick(user_data, ["user", "email", "admin"]),
+          date_expire: date_expire,
+        },
+        authSession
+      )
+    } catch (err) {
+      logoutUser(leaveSession)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  return <>{children}</>
+}
+
+const mapStateToProps = state => ({
+  UserSession: state.UserSession,
+})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ authSession, leaveSession }, dispatch),
+})
+
+AuthChecker.propTypes = {
+  actions: PropTypes.shape({
+    authSession: PropTypes.func.isRequired,
+    leaveSession: PropTypes.func.isRequired,
+  }).isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthChecker)
