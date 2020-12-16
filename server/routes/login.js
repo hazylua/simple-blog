27d3db8c9f7d57378e6a9a4367e2f52ce02fa688
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-15 23:09:53 -0300

    Add auth check to login get

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,11 +4,16 @@ const router = express.Router()
 const { validateUserLogin } = require("../models/user")
 const loginController = require("../controllers/login-controller")
 const validateMiddleware = require("../middleware/joi-validator")
+const authenticationMiddleware = require("../middleware/authentication")
 
 router.post(
   "/",
   validateMiddleware(validateUserLogin),
   loginController.userLogin
 )
+
+router.get("/", authenticationMiddleware(), async (req, res) => {
+  res.status(200).send(req.user)
+})
 
 module.exports = router
