6a3238c852f531ec199e959ec4e9749061f252c4
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-16 23:33:44 -0300

    Use snackbar to notify if no auth

--- src/components/Auth/PrivateRoute.jsx
+++ src/components/Auth/PrivateRoute.jsx
@@ -1,16 +1,22 @@
 import React from "react"
 import { navigate } from "gatsby"
 
+import { addSnackbar, leaveSession } from "src/store/actions"
+import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 
 import { tokenCheck } from "src/services/user-auth"
+import { notify } from "src/services/snackbar-notify"
 
-const PrivateRoute = ({ UserSession, children }) => {
+const PrivateRoute = ({ UserSession, actions, children }) => {
   const checkAuth = async () => {
+    const { leaveSession, addSnackbar } = actions
     try {
       if (UserSession.auth) {
         const response = await tokenCheck()
         if (!response) {
+          leaveSession()
+          notify("No auth.", addSnackbar, "middle", 2000)
           navigate("/")
           return null
         }
@@ -25,10 +31,16 @@ const PrivateRoute = ({ UserSession, children }) => {
   }
 
   if (checkAuth()) return <div>{children}</div>
+  else return null
+  // return <>{checkAuth() ? <div>{children}</div> : <span>Loading...</span>} </>
 }
 
 const mapStateToProps = state => ({
   UserSession: state.UserSession,
 })
 
-export default connect(mapStateToProps, null)(PrivateRoute)
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ leaveSession, addSnackbar }, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PrivateRoute)
